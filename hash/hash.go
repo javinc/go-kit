5fc95b5b7224b1208bc59c072ab377f326d13a12
javinc <javinczki02@gmail.com>
2018-01-23 23:49:31 +0800

    fix method names

--- hash/hash.go
+++ hash/hash.go
@@ -9,16 +9,16 @@ import (
 // Salt for hasing
 const Salt = "andpepper"
 
-// Hash string
-func Hash(s string) string {
+// Md5 string
+func Md5(s string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(s + Salt))
 
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-// GenerateHash base on timestamp
-func GenerateHash() string {
+// GenerateMd5 base on timestamp
+func GenerateMd5() string {
 	hasher := md5.New()
 	hasher.Write([]byte(time.Now().String() + Salt))
 
